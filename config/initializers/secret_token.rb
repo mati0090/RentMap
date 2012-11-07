a7c84decd249bdc5f6b8d1c9370e25ddcce1d9cd
Bury <mati0090@gmail.com>
2012-11-07 21:39:37 +0100

    Lets start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentMap::Application.config.secret_token = 'b2a183f5d791f9cc2ddcc867dc3b9821540d1d64310f51eb896714b1987620b5571d1c09d0196dfbcc768654d9b17c38df2aa094f1eec86b006dbbe9d636dc99'
